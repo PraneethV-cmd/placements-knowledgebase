92e8a88f59263ebce26033a489bf762b393721f8
PraneethV-cmd <praneethv375@gmail.com>
2023-10-31 20:55:07 +0530

    Added the patch 3 of the models.py

--- amrita-place/models.py
+++ amrita-place/models.py
@@ -1,42 +1,8 @@
-from sqlalchemy import Column, Integer, String, DateTime, Text, ForeignKey
-from sqlalchemy.orm  import relationship, Mapped, mapped_column
-from database import Base
-from sqlalchemy.sql import func
-
-# Example Classes for creating tables, and PK, FK relations
-
-class Student(Base):
-    __tablename__ = 'students'
-    id = Column(Integer, primary_key=True)
-    firstname = Column(String(100), nullable=False)
-    lastname = Column(String(100), nullable=False)
-    email = Column(String(80), unique=True, nullable=False)
-    age = Column(Integer)
-    created_at = Column(DateTime(timezone=True),
-                           server_default=func.now())
-    bio = Column(Text)
-    course_id = mapped_column(ForeignKey("courses.course_id")) # Child table 
-
-    def __init__(self, firstname, lastname): # constructor. Defines what to do when you do stu = Student("Bob", "Morly", ...)
-        self.firstname = firstname
-
-
-    def __repr__(self): # this tells python what to print if you do print(student) where student is a Student object.
-        return f'<Student {self.firstname}>'
-    
-class Courses(Base):
-    __tablename__ = "courses"
-    course_id = Column(Integer, primary_key=True)
-    course_name = Column(String(100), nullable=False)
-    children = relationship() # Parent table
-  # add __init__ and __repr__ definitions to all classes
-
-
-#to be edited by me(adi) n vatsu n nigga
 from sqlalchemy import Column, Integer, String, DateTime, Text, ForeignKey, Boolean
-from sqlalchemy.orm import relationship
-from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship, Mapped, mapped_column
 from database import Base
+from sqlalchemy.sql import func
+
 
 class Student(Base):
     __tablename__ = 'students'
@@ -47,25 +13,49 @@ class Student(Base):
     age = Column(Integer)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     bio = Column(Text)
-    course_id = Column(Integer, ForeignKey("courses.course_id")) # Child table 
+    course_id = mapped_column(ForeignKey("courses.course_id"))
 
-    def __init__(self, firstname, lastname): # constructor. Defines what to do when you do stu = Student("Bob", "Morly", ...)
+    courses = relationship("Courses", back_populates="students")
+    phone_numbers = relationship("PhoneNumber", cascade="all, delete-orphan", back_populates="student")
+    interview_experiences = relationship("InterviewExperience", backref="student")
+    student_degree_holders = relationship("StudentDegreeHolder", cascade="all, delete-orphan", back_populates="student")
+
+    def __init__(self, firstname, lastname):
         self.firstname = firstname
         self.lastname = lastname
 
-    def __repr__(self): # this tells python what to print if you do print(student) where student is a Student object.
-        return f'<Student {self.firstname}>'
+    def __repr__(self):
+        return f'<Student {self.id}>'
+
 
 class Courses(Base):
     __tablename__ = "courses"
     course_id = Column(Integer, primary_key=True)
     course_name = Column(String(100), nullable=False)
-    students = relationship("Student", backref="courses") # Parent table
+
+    students = relationship("Student", back_populates="courses")
+
+    def __init__(self, course_name):
+        self.course_name = course_name
+
+    def __repr__(self):
+        return f'<Course {self.course_id}>'
+
 
 class PhoneNumber(Base):
     __tablename__ = "phone_number"
-    roll_number = Column(Integer, ForeignKey("students.id"), primary_key=True)
-    phone_number = Column(String(20), primary_key=True)
+    id = Column(Integer, primary_key=True)
+    roll_number = Column(Integer, ForeignKey("students.id"))
+    phone_number = Column(String(20))
+
+    student = relationship("Student", back_populates="phone_numbers")
+
+    def __init__(self, phone_number):
+        self.phone_number = phone_number
+
+    def __repr__(self):
+        return f'<PhoneNumber {self.phone_number}>'
+
 
 class Company(Base):
     __tablename__ = "company"
@@ -73,11 +63,31 @@ class Company(Base):
     logo = Column(String(255))
     name = Column(String(100), nullable=False)
 
+    interview_experiences = relationship("InterviewExperience", backref="company")
+
+    def __init__(self, name, logo):
+        self.name = name
+        self.logo = logo
+
+    def __repr__(self):
+        return f'<Company {self.company_id}>'
+
+
 class Degree(Base):
     __tablename__ = "degree"
     program_id = Column(Integer, primary_key=True)
     name = Column(String(100), nullable=False)
     branch = Column(String(100))
+
+    student_degree_holders = relationship("StudentDegreeHolder", cascade="all, delete-orphan", back_populates="degree")
+
+    def __init__(self, name, branch):
+        self.name = name
+        self.branch = branch
+
+    def __repr__(self):
+        return f'<Degree {self.program_id}>'
+
 
 class InterviewExperience(Base):
     __tablename__ = "interview_experience"
@@ -88,18 +98,13 @@ class InterviewExperience(Base):
     roll_number = Column(Integer, ForeignKey("students.id"))
     company_id = Column(Integer, ForeignKey("company.company_id"))
 
-class Administrator(Base):
-    __tablename__ = "administrator"
-    admin_id = Column(Integer, primary_key=True)
-    username = Column(String(100), nullable=False)
-    password_hash = Column(String(255), nullable=False)
-    name = Column(String(100), nullable=False)
+    student = relationship("Student", back_populates="interview_experiences")
+    company = relationship("Company", back_populates="interview_experiences")
 
-class StudentDegreeHolder(Base):
-    __tablename__ = "student_degree_holder"
-    roll_number = Column(Integer, ForeignKey("students.id"), primary_key=True)
-    program_id = Column(Integer, ForeignKey("degree.program_id"), primary_key=True)
+    def __init__(self, positive_points, is_job_secured, improvements):
+        self.positive_points = positive_points
+        self.is_job_secured = is_job_secured
+        self.improvements = improvements
 
-
-
-    
+    def __repr__(self):
+        
